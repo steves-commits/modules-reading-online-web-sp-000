a5abf4bff56a8a38c3ac76a9f059c100d7684519
Steven Topazio <topazio.steven@gmail.com>
2020-11-20 21:27:57 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,4 +3,12 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+  
+  class Dancer
+    extend MedaDancing
+  end
+  
+  class Kid 
+    extend MetaDancing 
+  end 
 end
