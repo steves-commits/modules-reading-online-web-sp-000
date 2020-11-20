073e9f25115f1c60c5f6162769abea654218eb66
Steven Topazio <topazio.steven@gmail.com>
2020-11-20 21:34:30 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,8 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer 
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
-end 
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
 
